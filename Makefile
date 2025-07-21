0c4a17124ce2be9017755e7e3756a3a9b7149bc2
NobleNomadic <noblenomadic@proton.me>
2025-07-22 07:34:13 +0930

    Fixed folder structure

--- Makefile
+++ Makefile
@@ -2,75 +2,33 @@ ASM = nasm
 
 SRC_DIR = src
 BUILD_DIR = build
+BOOT_DIR = boot
 
-BOOT_SRC = $(SRC_DIR)/boot/boot.asm
-BMANAGE_SRC = $(SRC_DIR)/boot/bootmanage.asm
-KERNEL_SRC = $(SRC_DIR)/kernel/kernel.asm
-KERNELLIB_SRC = $(SRC_DIR)/kernel/kernellib.asm
-BASIC_PROGRAM_SRC = $(SRC_DIR)/programs/basic.asm
-SHELL_SRC = $(SRC_DIR)/shell/shell.asm
-NNFS_SRC = $(SRC_DIR)/fs/nnfs.asm
-
-BOOT_BIN = $(BUILD_DIR)/boot/boot.bin
-BMANAGE_BIN = $(BUILD_DIR)/boot/bootmanage.bin
-KERNEL_BIN = $(BUILD_DIR)/kernel/kernel.bin
-KERNELLIB_BIN = $(BUILD_DIR)/kernel/kernellib.bin
-BASIC_PROGRAM_BIN = $(BUILD_DIR)/programs/basic.bin
-SHELL_BIN = $(BUILD_DIR)/shell/shell.bin
-NNFS_BIN = $(BUILD_DIR)/fs/nnfs.bin
-
+BOOT_SRC = $(SRC_DIR)/$(BOOT_DIR)/boot.asm
+BOOT_BIN = $(BUILD_DIR)/$(BOOT_DIR)/boot.bin
 IMG = $(BUILD_DIR)/os.img
 
+# Default target
 all: $(IMG)
 
-# Assemble boot.asm
+# Assemble the bootloader to exactly 512 bytes
 $(BOOT_BIN): $(BOOT_SRC)
-	mkdir -p $(BUILD_DIR)/boot
-	$(ASM) $< -f bin -o $@
+	mkdir -p $(BUILD_DIR)/$(BOOT_DIR)
+	$(ASM) -f bin $< -o $@
+	@ actual_size=$$(stat -c%s $@); \
+	   if [ $$actual_size -ne 512 ]; then \
+	       echo "Error: Boot sector must be exactly 512 bytes (was $$actual_size)"; \
+	       exit 1; \
+	   fi
 
-# Assemble bootmanage.asm
-$(BMANAGE_BIN): $(BMANAGE_SRC)
-	mkdir -p $(BUILD_DIR)/boot
-	$(ASM) $< -f bin -o $@
-
-# Assemble kernel.asm
-$(KERNEL_BIN): $(KERNEL_SRC)
-	mkdir -p $(BUILD_DIR)/kernel
-	$(ASM) $< -f bin -o $@
-
-# Assemble kernellib.asm
-$(KERNELLIB_BIN): $(KERNELLIB_SRC)
-	mkdir -p $(BUILD_DIR)/kernel
-	$(ASM) $< -f bin -o $@
-
-# Assemble shell.asm
-$(SHELL_BIN): $(SHELL_SRC)
-	mkdir -p $(BUILD_DIR)/shell
-	$(ASM) $< -f bin -o $@
-
-# Assemble nnfs.asm
-$(NNFS_BIN): $(NNFS_SRC)
-	mkdir -p $(BUILD_DIR)/fs
-	$(ASM) $< -f bin -o $@
-
-# Assemble basic.asm
-$(BASIC_PROGRAM_BIN): $(BASIC_PROGRAM_SRC)
-	mkdir -p $(BUILD_DIR)/programs
-	$(ASM) $< -f bin -o $@
-
-# Build floppy image
-$(IMG): $(BOOT_BIN) $(BMANAGE_BIN) $(KERNEL_BIN) $(KERNELLIB_BIN) $(SHELL_BIN) $(NNFS_BIN) $(BASIC_PROGRAM_BIN)
+# Create floppy disk image with bootloader
+$(IMG): $(BOOT_BIN)
+	mkdir -p $(BUILD_DIR)
 	cp $(BOOT_BIN) $(IMG)
-	dd if=$(BMANAGE_BIN) of=$(IMG) bs=512 seek=1 conv=notrunc
-	dd if=$(KERNEL_BIN) of=$(IMG) bs=512 seek=5 conv=notrunc
-	dd if=$(KERNELLIB_BIN) of=$(IMG) bs=512 seek=12 conv=notrunc
-	dd if=$(SHELL_BIN) of=$(IMG) bs=512 seek=16 conv=notrunc
-	dd if=$(NNFS_BIN) of=$(IMG) bs=512 seek=39 conv=notrunc
-	dd if=$(BASIC_PROGRAM_BIN) of=$(IMG) bs=512 seek=49 conv=notrunc
-
 	truncate -s 1440k $(IMG)
 
+# Clean build directory
 clean:
-	rm -rf $(BUILD_DIR)/*
+	rm -rf $(BUILD_DIR)
 
 .PHONY: all clean
--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,76 +1,80 @@
-; boot.asm - Inital bootloader to load the boot manager
-[bits 16]
+; boot.asm - Initial bootloader
 [org 0x7C00]
-; Macro for end of string with newline
+[bits 16]
+
 %define STREND 0x0D, 0x0A, 0x00
+
 ; Entry point
-start:
-  ; Setup data
-  mov ax, 0
+bootEntry:
+  ; Setup data segments
+  xor ax, ax
   mov ds, ax
-  ; Setup stack
-  mov ss, ax
-  mov sp, 0x7C00
-  ; Clear the screen with BIOS interupt to reset mode
-  mov ah, 0x00 ; Setup font and tty data
-  mov al, 0x03
-  int 0x10     ; Call BIOS interupt
-  ; Print msg variable
-  mov si, msg    ; Load message into SI
-  call printString
-  ; Load the kernel, code should not return from here
-  jmp loadBootManage
-
-; Load the second part of the bootloader from the disk into memory and give it control
-loadBootManage:
-  mov ax, 0x0000      ; Segment 0x0000
   mov es, ax
-  mov bx, 0x8000      ; Load to 0000:8000 (physical 0x8000)
-  mov ah, 0x02        ; BIOS: read sectors
-  mov al, 4           ; Read 4 sectors
-  mov ch, 0           ; Cylinder 0
-  mov cl, 2           ; Start from sector 2 (sector 1 is boot)
-  mov dh, 0           ; Head 0
-  mov dl, 0x00        ; Drive 0 (floppy)
-  int 0x13            ; Call BIOS
-  jc .diskReadFail    ; Jump if carry (error)
-  ; Fully jump to 0000:8000 and give that code control
-  jmp 0x0000:0x8000
 
-; Handle a failed disk read
-.diskReadFail:
-  mov si, diskErr
-  call printString
-  jmp hang
-
-; Print a string stored in SI
-printString:
-  push ax
-  push si
-
-.printLoop:
-  lodsb
-  or al, al
-  jz .done
-
-  mov ah, 0x0E
+  ; Clear screen
+  mov ah, 0x00
+  mov al, 0x03        ; 80x25 color text mode
   int 0x10
 
-  jmp .printLoop
+  ; Print string to show loaded
+  mov si, bootEntryMsg
+  call printString
 
+  ; Move to loading the boot manager
+  jmp loadBootManage
+
+; Print the string stored in SI
+printString:
+  push ax     ; Push used registers
+  push si
+.printLoop
+  lodsb          ; Load next
+  or al, al      ; Check for null terminator
+  jz .done       ; Finish if it is
+  mov ah, 0x0E   ; BIOS print tty
+  int 0x10       ; Interupt
+  jmp .printLoop ; Continue loop
 .done:
-  pop si
+  pop si       ; Return used registers and return
   pop ax
   ret
 
-; Hang program
+loadBootManage:
+  ; Memory arguments
+  mov ax, 0x0000   ; Segment
+  mov es, ax
+  mov bx, 0x2000   ; Offset
+  ; Disk arguments
+  mov al, 4        ; Read 4 sectors
+  mov ch, 0        ; Cylinder 0
+  mov cl, 2        ; Sector 2 (read 2-5)
+  mov dh, 0        ; Head 0
+  mov dl, 0x00     ; Drive 0
+
+  ; Call BIOS
+  mov ah, 0x02     ; BIOS read sectors
+  int 0x13
+
+  ; Check for error
+  jc .diskReadFail
+
+  ; Jump to loaded address
+  jmp 0x0000:0x2000
+
+; Print error message and hang
+.diskReadFail:
+  mov si, failedLoadMsg
+  call printString
+  jmp hang
+
 hang:
-  jmp $ ; Jump to the current location in memory
+  jmp $
 
-; DATA SECTION
-msg db "[*] Bootable device found", STREND
-diskErr db "[!] Disk read failed - 2", STREND
 
-; Boot signature
+; Data section
+bootEntryMsg db "[*] Bootable device found", STREND ; Print to show bootloader is bootable
+failedLoadMsg db "[-] Disk read failed - 2", STREND ; Print if the disk failed to read
+
+; Pad and add boot signature
 times 510 - ($ - $$) db 0
 dw 0xAA55
