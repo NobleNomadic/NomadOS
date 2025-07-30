55330e767b2da9449ec428d0daf27b4693be848d
NobleNomadic <noblenomadic@proton.me>
2025-07-31 07:34:07 +0930

    Working input syscall

--- src/kernel/kernel.asm
+++ src/kernel/kernel.asm
@@ -16,6 +16,9 @@ kernelEntry:
   cmp bl, 1              ; Syscall 1: Print string for user programs in sector 0x2000
   je .printStringHandler
 
+  cmp bl, 2              ; Syscall 2: Get a line of input for user programs
+  je .getInputHandler
+
   ; No syscall, return far across segment
   retf
 
@@ -28,6 +31,14 @@ kernelEntry:
   mov es, ax
   call printString ; Call print function
   retf             ; Return far across segment
+
+.getInputHandler:
+  ; Move to userland segment
+  mov ax, 0x2000
+  mov ds, ax
+  mov es, ax
+  call getInput
+  retf
 
 ; SYSCALLS
 ; ;;;;;;;
@@ -46,7 +57,7 @@ kernelSetup:
   jmp 0x2000:0x0000
 
 
-; Syscall 1: print string in SI
+; Syscall 1: Print string in SI
 printString:
   push ax        ; Push used registers
   push si
@@ -58,6 +69,48 @@ printString:
   int 0x10       ; Call BIOS
   jmp .printLoop ; Continue loop
 .done:
+  pop si         ; Return register state and return
+  pop ax
+  ret
+
+; Syscall 2: Input function to return a string in SI
+getInput:
+  push ax        ; Push used registers
+  push si
+.inputLoop:
+  ; Use BIOS for getting a key of input
+  mov ah, 0x00   ; BIOS blocking input
+  int 0x16       ; Call BIOS interrupt
+
+  ; Check if the byte written to AL was enter key (0x0D)
+  cmp al, 0x0D
+  je .done
+
+  ; Write the character to SI
+  mov [si], al
+  inc si
+
+  ; Echo the character back
+  mov ah, 0x0E   ; BIOS tty print
+  int 0x10       ; Call interrupt for tty print
+  jmp .inputLoop ; Continue getting input
+
+.done:
+  ; Add newline and null terminator to SI
+  mov byte [si], 0x0D
+  inc si
+  mov byte [si], 0x0A
+  inc si
+  mov byte [si], 0x00
+  inc si
+
+  ; Print newline after enter
+  mov al, 0x0D
+  mov ah, 0x0E
+  int 0x10
+  mov al, 0x0A
+  int 0x10
+  
   pop si         ; Return register state and return
   pop ax
   ret
--- src/programs/shell.asm
+++ src/programs/shell.asm
@@ -1,5 +1,6 @@
 ; shell.asm - Default userspace entry program to provide a basic command line
 [org 0x0000]
+[bits 16]
 
 %define STREND 0x0D, 0x0A, 0x00
 
@@ -10,17 +11,42 @@ shellEntry:
   mov es, ax
 
   ; Test syscall 1 in kernel
-  mov si, syscallTest
+  mov si, shellEntryMsg
   mov byte bl, 1
   ;CALL_kernel
 
+  ; Go to the main shell loop
+  jmp shellLoop
+
+
+shellLoop:
+  ; Print prompt
+  mov si, shellPromptMsg
+  mov byte bl, 1
+  ;CALL_kernel
+
+  ; Get a line of input with syscall 2 into buffer
+  mov si, buffer
+  mov byte bl, 2
+  ;CALL_kernel
+
+  ; Echo the message
+  mov si, buffer
+  mov byte bl, 1
+  ;CALL_kernel
+
+  ; Continue loop
+  jmp shellLoop
 
 ; Fallback hang function
 hang:
   jmp $
 
 ; DATA SECTION
-syscallTest db "[+] Kernel syscalls setup", STREND
+shellEntryMsg db "[+] Userpace syscalls setup", STREND ; Message to show that syscalls are working
+shellPromptMsg db "[>]", STREND ; Shell prompt message
+
+buffer times 256 db 0 ; Buffer for input
 
 ; Pad to 4 sectors
 times 2048 - ($ - $$) db 0
