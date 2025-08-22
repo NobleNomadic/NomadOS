06db169bb43489adf58eda1d9ecce5c393565348
NobleNomadic <noblenomadic@proton.me>
2025-08-23 08:06:28 +0930

    Licensed under GPL v2

--- src/util/killscreen.pp.asm
+++ src/util/killscreen.pp.asm
@@ -1,3 +1,21 @@
+;
+; NomadOS - 16 bit hobby operating system
+;
+; Copyright (C) 2025 NobleNomadic
+;
+; This program is free software; you can redistribute it and/or
+; modify it under the terms of the GNU General Public License
+; as published by the Free Software Foundation; either version 2
+; of the License, or (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, see <http://www.gnu.org/licenses/>.
+;
 ; killscreen.asm - When this code is run, all processes and current will stop (BSoD equivalent)
 ; Triggered at any stage by running jmp 0x0000:0x1000 to kill OS as fast as possible
 [org 0x1000]
